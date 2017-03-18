47548c3492899a9321f1708e4093b6980dfa7e3e
Jinn Jin <jinnjin@Jinns-MacBook-Pro.local>
2017-03-18 10:28:12 +0800

    subtract, multiply and divide

--- algebra/0_10.py
+++ algebra/0_10.py
@@ -5,3 +5,15 @@ count = 10
 while count > 0:
 	print "%d + %d = " % (random.randint(0, 10), random.randint(0, 10))
 	count -= 1
+
+while count < 10:
+	print "%d - %d = " % (random.randint(0, 10), random.randint(0, 10))
+	count += 1
+
+while count > 0:
+	print "%d x %d = " % (random.randint(0, 10), random.randint(0, 10))
+	count -= 1
+
+while count < 10:
+	print "%d / %d = " % (random.randint(0, 10), random.randint(1, 10))
+	count += 1
