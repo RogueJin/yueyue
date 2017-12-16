3ef5de52398c8c81791853fbef0b3473650c1b83
Jinn Jin <rogue_king@hotmail.com>
2017-12-16 17:29:46 +0800

    modify 0_100 to avoid negtive result

--- algebra/0_100.py
+++ algebra/0_100.py
@@ -5,13 +5,29 @@ count = 20
 operators = ["+", "-", "x", "/"]
 
 while count > 0:
-	nums = random.randint(2,4)
-	equation = str(random.randint(0, max_num))
-
-	for i in range(1, nums):
+	totalnums = random.randint(2,4)
+	result = random.randint(0, max_num)
+	equation = str(result)
+	
+	for i in range(1, totalnums):
+		num = random.randint(0, max_num)
 		operator = operators[random.randint(0,1)]
+
+		if operator == '+':
+			result += num
+		elif operator == '-':
+			result -= num
+			if result < 0:
+				operator = "+"
+				result += num * 2
+
+		elif operator == '*':
+			result *= num
+		elif operator == '/':
+			result /= num
+
 		equation += operator
-		equation += str(random.randint(0, max_num))
+		equation += str(num)
 
 	print "%s = " % (equation)
 	count -= 1
